d4e35ec18ac2724b4d34bdfa06e4804053684704
pavanramkalyan-debug <pavanramkalyanboorla@gmail.com>
2026-03-18 00:05:08 +0530

    src03 and data01

--- src/ingestion/data_ingestion.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from src.utils.exceptions import CustomException
-from src.utils.logger import logging
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
-
